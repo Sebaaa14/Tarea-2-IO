608870f2207c69bf975a5e6ed591e9378ec9ad0e
Seba <sebazxx14@gmail.com>
2023-11-04 20:55:48 -0300

    Co-authored-by: IgnacioCabreraS <IgnacioCabreraS@users.noreply.github.com> Co-authored-by: Gabriel Pizarro <Gabossj@users.noreply.github.com> Co-authored-by: Acertijook <marr109@users.noreply.github.com>

---
+++ code.py
@@ -0,0 +1,39 @@
+entero = 50
+
+with open ("QAP_sko56_04_n") as files:
+#from ("QAP_sko56_04_n") import files
+
+# Carga el archivo QAP_sko56_04_n.txt desde tu computadora local al entorno de Colab
+uploaded = files.upload()
+
+# Abre el archivo
+with open("S8", 'r') as archivo:
+    # Tu código para procesar el archivo
+
+
+# Lee todas las líneas del archivo
+    lineas = archivo.readlines()
+# Extrae la cantidad de puestos del primer elemento
+cantidad_puestos = int(lineas[0])
+
+# Extrae los tamaños de los puestos de la segunda línea
+tamaños_puestos = list(map(int, lineas[1].split(',')))
+
+
+# Inicializa una matriz para almacenar los datos de clientes
+clientes = []
+for linea in lineas[2:]:
+    valores = linea.split(',')
+    fila = list(map(int, valores))
+    clientes.append(fila)
+
+# Imprime la cantidad de puestos
+print("Cantidad de puestos:", cantidad_puestos)
+
+#Imprime los tamaños de los puestos
+print("Tamaños de los puestos:", tamaños_puestos)
+
+# Imprime los datos de clientes
+print("Datos de clientes:")
+for fila in clientes:
+    print(fila)
