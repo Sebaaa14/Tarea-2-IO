3dc3edbbae893fcf55fec21d1cc0ed762c3bc641
Seba <sebazxx14@gmail.com>
2023-11-04 22:03:07 -0300

    Nuevos archivos

--- codigo.py
+++ codigo.py
@@ -1,37 +1,28 @@
-with open ("QAP_sko56_04_n") as files:
-#from ("QAP_sko56_04_n") import files
+import re
 
-# Carga el archivo QAP_aasdsdasdsko56_04_n.txt desde tu computadora local al entorno de Colab
-uploaded = files.upload()
+# Especifica la ruta al archivo en tu sistema local
+archivo_path = "S5.txt"  # Reemplaza con la ruta real del archivo
 
-# Abre el archivo
-with open("S8", 'r') as archivo:
-    # Tu código para procesar el archivo
-
-
-# Lee todas las líneas del archivo
+# Abre el archivo en modo lectura
+with open(archivo_path, 'r') as archivo:
+    # Lee todas las líneas del archivo
     lineas = archivo.readlines()
+
 # Extrae la cantidad de puestos del primer elemento
 cantidad_puestos = int(lineas[0])
 
 # Extrae los tamaños de los puestos de la segunda línea
 tamaños_puestos = list(map(int, lineas[1].split(',')))
 
-
-# Inicializa una matriz para almacenar los datos de clientes
-clientes = []
+# Extrae la cantidad de clientes estimados entre pares de puestos en una matriz simétrica
+clientes_estimados = []
 for linea in lineas[2:]:
-    valores = linea.split(',')
-    fila = list(map(int, valores))
-    clientes.append(fila)
+    valores = list(map(int, linea.split(',')))
+    clientes_estimados.append(valores)
 
-# Imprime la cantidad de puestos
+# Imprime la información
 print("Cantidad de puestos:", cantidad_puestos)
-
-#Imprime los tamaños de los puestos
 print("Tamaños de los puestos:", tamaños_puestos)
-
-# Imprime los datos de clientes
-print("Datos de clientes:")
-for fila in clientes:
+print("Clientes estimados entre pares de puestos:")
+for fila in clientes_estimados:
     print(fila)
